a9554c46bf9a67f391a63ab6b973b9c0bfed063f
rayyanekaputra <rayyan.ekaputra@gmail.com>
2025-01-05 16:22:45 +0800

    troubleshoot sticky navbar

--- static/mobile-menu.js
+++ static/mobile-menu.js
@@ -22,23 +22,36 @@ Viewport:
 +----------------------------+
     
     
-    
     */
 
-  $(window).scroll(function () {
-    //isInView funtion to detect inview by checking overlap
-    function isInView(element) {
-      const elementTop = $(element).offset().top;
-      const elementBottom = elementTop + $(element).outerHeight();
-      const viewportTop = $(window).scrollTop();
-      const viewportBottom = viewportTop + $(window).height();
+// //deteksi view
+//   $(window).scroll(function () {
+//     //isInView funtion to detect inview by checking overlap
+//     function isInView(element) {
+//       const elementTop = $(element).offset().top;
+//       const elementBottom = elementTop + $(element).outerHeight();
+//       const viewportTop = $(window).scrollTop();
+//       const viewportBottom = viewportTop + $(window).height();
 
-      return elementBottom > viewportTop && elementTop < viewportBottom;
+//       return elementBottom > viewportTop && elementTop < viewportBottom;
+//     }
+//     $("section").each(function () {
+//       if (isInView(this)) {
+//         console.log($(this).attr("class") + " " + "is in view");
+//       }
+//     });
+//   });
+
+  $(window).scroll(function () { 
+    const navbarOffset = $('.navbar').offset().top;
+    console.log("navbarOffset = " + navbarOffset)
+    console.log("scrollTop = " + $(window).scrollTop())
+    if($(window).scrollTop() >= navbarOffset){
+        
+        $('.navbar').addClass('sticky');
+    } else{
+        $('.navbar').removeClass('sticky');
     }
-    $("section").each(function () {
-      if (isInView(this)) {
-        console.log($(this).attr("class") + " " + "is in view");
-      }
-    });
+    
   });
 });
