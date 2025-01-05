97548f4b656982e8825a8b2e3557803beb688b41
rayyanekaputra <rayyan.ekaputra@gmail.com>
2025-01-05 16:42:48 +0800

    sticky navbar when work grid is in view, pretty fun

--- static/mobile-menu.js
+++ static/mobile-menu.js
@@ -24,31 +24,34 @@ Viewport:
     
     */
 
-// //deteksi view
-//   $(window).scroll(function () {
-//     //isInView funtion to detect inview by checking overlap
-//     function isInView(element) {
-//       const elementTop = $(element).offset().top;
-//       const elementBottom = elementTop + $(element).outerHeight();
-//       const viewportTop = $(window).scrollTop();
-//       const viewportBottom = viewportTop + $(window).height();
+  //deteksi view
+  $(window).scroll(function () {
+    //isInView funtion to detect inview by checking overlap
+    function isInView(element) {
+      const elementTop = $(element).offset().top;
+      const elementBottom = elementTop + $(element).outerHeight();
+      const viewportTop = $(window).scrollTop();
+      const viewportBottom = viewportTop + $(window).height();
 
-//       return elementBottom > viewportTop && elementTop < viewportBottom;
-//     }
-//     $("section").each(function () {
-//       if (isInView(this)) {
-//         console.log($(this).attr("class") + " " + "is in view");
-//       }
-//     });
-//   });
-
-  $(window).scroll(function () { 
-    if($(window).scrollTop() >= 200){
-        
-        $('.navbar').addClass('sticky');
-    } else{
-        $('.navbar').removeClass('sticky');
+      return elementBottom > viewportTop && elementTop < viewportBottom;
     }
-    
+    $("section").each(function () {
+      if (isInView($(this)) && $(this).hasClass("work")) {
+        console.log("im here");
+        $('.navbar').addClass('sticky');
+      } else {
+        $('.navbar').removeClass('sticky');
+      }
+    });
   });
+
+    $(window).scroll(function () {
+      if($(window).scrollTop() >= 700){
+
+          $('.navbar').addClass('sticky');
+      } else{
+          $('.navbar').removeClass('sticky');
+      }
+
+    });
 });
